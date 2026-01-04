cfc4fa4a991b16cf56f68f3a450809b6d93389e1
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-04 21:58:32 +0530

    added tailwind css

---
+++ React/7-tailwind-css/src/App.jsx
@@ -0,0 +1,10 @@
+// This is about that how we can attached the tailwind css by the vite
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 text-gray-100 text-6xl'>
+       Hii this is a div
+      </div>
+  )
+}
+
+export default App
