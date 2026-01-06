0da7e5780f274ef754fd6551ece05ab1eaf1ed06
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-06 14:25:42 +0530

    added react Ui project

--- React/React-Projects/8-Ui-project/src/App.jsx
+++ React/React-Projects/8-Ui-project/src/App.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import Section1 from "./components/Section-1/Section-1";
-import Section2 from "./components/Section-2/Section-2";
 
 const App = () => {
   const users = [
@@ -30,7 +28,6 @@ const App = () => {
   return (
     <div>
       <Section1 users = {users}/>
-      <Section2 />
     </div>
   );
 };
