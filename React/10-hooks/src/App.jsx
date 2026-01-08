e2f25f6b5c23033463beb3783cac8e4e3c83a4fc
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-08 22:33:12 +0530

    added hooks

---
+++ React/10-hooks/src/App.jsx
@@ -0,0 +1,21 @@
+const App = () => {
+  return (
+    <div className="div">
+      <ul>
+        <h1>
+          Hooks are special funtions (magical fucnctions) that do differnt
+          differnt tasks, and they are as follows:-
+        </h1>
+        <li>useState</li>
+        <li>useEffect</li>
+        <li>useContext</li>
+        <li>useReducer</li>
+        <li>useRef</li>
+        <li>useMemo</li>
+        <li>useCallback</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
