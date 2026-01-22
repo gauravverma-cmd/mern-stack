a1b8db5c2cd1d43102d81ebbfee65f46061ad83a
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-22 12:30:59 +0530

    added the routing-dom

---
+++ React/22-routing-dom/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ React/22-routing-dom/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <h2>Atlatis</h2>
+      <div className="nav-left">
+        <Link to="/">Home</Link>
+        <Link to="/about">AboutUs</Link>
+        <Link to="/product">Product</Link>
+        <Link to="/contact">ContactUs</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
