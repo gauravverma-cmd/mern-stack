c3739caff1db9290bd23d5360fc292a2832daab7
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-27 22:40:03 +0530

    Reset git count

---
+++ React/React-Projects/26-weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen w-full'>
+      <div>
+        <h1 className='text-white'>hii this gaurav verma  </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
