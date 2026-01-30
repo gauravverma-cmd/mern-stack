a35ebbcd13d771024c4e05766bfddba008c97b88
gauravverma-cmd <gauravverma250609@gmail.com>
2026-01-30 19:34:34 +0530

    add wap

--- React/React-Projects/26-weather-app/src/App.jsx
+++ React/React-Projects/26-weather-app/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
     <div className='bg-black h-screen w-full'>
       <div>
         <h1 className='text-white'>hii this gaurav verma  </h1>
+        <h1 className='text-white bg-amber-800  w-fit rounded-2xl p-3'>This is a heading</h1>
+        <h1>
+          Done 
+        </h1>
       </div>
     </div>
   )
