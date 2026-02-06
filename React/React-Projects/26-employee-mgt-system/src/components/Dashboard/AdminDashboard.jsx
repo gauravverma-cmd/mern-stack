7cbbd9952ba22c3306a500b386c68787db2bd130
gauravverma-cmd <gauravverma250609@gmail.com>
2026-02-06 17:13:19 +0530

    added the ems

--- React/React-Projects/26-employee-mgt-system/src/components/Dashboard/AdminDashboard.jsx
+++ React/React-Projects/26-employee-mgt-system/src/components/Dashboard/AdminDashboard.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import Header from "../Others/Header";
+import CreateTask from "../Others/CreateTask";
+import AllTask from "../Others/AllTask";
 
 const AdminDashboard = () => {
   return (
-    <div className="h-screen w-full p-10">
+    <div className="h-screen w-full p-6">
       <Header />
-      <div>
-        <form>
-          <h3>Task Title</h3>
-          <input type="text" placeholder="Enter the task title" />
-          <h3>Description</h3>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-          <h3>Date</h3>
-          <input type="date" name="" id="" />
-          <h3>Assign to</h3>
-          <input type="text" placeholder="Employee name" />
-          <h3>Category</h3>
-          <input type="text" placeholder="Design,Webdev,editor,etc...."/>
-          <button>Create Task</button>
-        </form>
-      </div>
+      <CreateTask />
+      <AllTask />
     </div>
   );
 };
---
+++ React/React-Projects/26-employee-mgt-system/src/components/Others/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='AllTask' className='bg-[#1c1c1c] mt-5 p-5 overflow-auto h-48 rounded'>
+        <div className='bg-red-500 mb-2 flex justify-between px-4 py-2 rounded'>
+            <h2>Alise</h2>
+            <h2>Make a ui</h2>
+            <h2>Status</h2>
+        </div>
+        <div className='bg-green-500 mb-2 flex justify-between px-4 py-2 rounded'>
+            <h2>Alise</h2>
+            <h2>Make a ui</h2>
+            <h2>Status</h2>
+        </div>
+        <div className='bg-yellow-500 mb-2 flex justify-between px-4 py-2 rounded'>
+            <h2>Alise</h2>
+            <h2>Make a ui</h2>
+            <h2>Status</h2>
+        </div>
+        <div className='bg-blue-500 mb-2 flex justify-between px-4 py-2 rounded'>
+            <h2>Alise</h2>
+            <h2>Make a ui</h2>
+            <h2>Status</h2>
+        </div>
+        <div className='bg-cyan-500 mb-2 flex justify-between px-4 py-2 rounded'>
+            <h2>Alise</h2>
+            <h2>Make a ui</h2>
+            <h2>Status</h2>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
