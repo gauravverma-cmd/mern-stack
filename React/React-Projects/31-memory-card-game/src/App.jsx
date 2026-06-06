84e535de28d2c2afd0034cce4309b2c3578a42b4
gauravverma-cmd <gauravverma250609@gmail.com>
2026-06-06 14:08:40 +0530

    memory game update

--- React/React-Projects/31-memory-card-game/src/App.jsx
+++ React/React-Projects/31-memory-card-game/src/App.jsx
@@ -4,45 +4,61 @@ import GameHeader from "./components/GameHeader";
 
 const cardValues = [
   "🥝",
-    "🍇",
-    "🍉",
-    "🍌",
-    "🍓",
-    "🍒",
-    "🍎",
-    "🍍",
-    "🥝", 
-    "🍇",
-    "🍉",
-    "🍌",
-    "🍓",
-    "🍒",
-    "🍎",
-    "🍍"
+  "🍇",
+  "🍉",
+  "🍌",
+  "🍓",
+  "🍒",
+  "🍎",
+  "🍍",
+  "🥝",
+  "🍇",
+  "🍉",
+  "🍌",
+  "🍓",
+  "🍒",
+  "🍎",
+  "🍍",
 ];
 
 const App = () => {
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
 
- const intitailizeGame = ()=> {
+  const intitailizeGame = () => {
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+    setCards(finalCards);
+  };
+  useEffect(() => {
+    intitailizeGame();
+  }, []);
 
- const finalCards =  cardValues.map((value, index)=>({
-        id: index,
-        value,
-        idFlipped: false,
-        isMatched: false,
-      }))
-        setCards(finalCards)
-    };
-      useEffect(()=>{
-         intitailizeGame()
-      },[])
+  const handleCardClick = (card) => {
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+  };
+
   return (
     <div className="app">
       <GameHeader score={4} move={23} />
       <div className="cards-grid">
         {cards.map((card) => (
-          <Card card={card} />
+          <Card key={card.id} card={card} onClick={handleCardClick} />
         ))}
       </div>
     </div>
