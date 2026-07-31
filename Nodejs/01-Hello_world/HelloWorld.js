6c318e0467198173e60d9d2d8752c53b426134f1
gauravverma-cmd <gauravverma250609@gmail.com>
2026-07-31 10:50:03 +0530

    statring the node.js

---
+++ Nodejs/01-Hello_world/HelloWorld.js
@@ -0,0 +1,2 @@
+console.log("Hii there ! its my first program in node js")
+// also i made a script in package.json named start by which we can run our code just by giving the command npm start in terminal.
