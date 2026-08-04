ad4d7f0ecac919c552918c914a04f6e8e61abe8a
gauravverma-cmd <gauravverma250609@gmail.com>
2026-08-04 13:46:39 +0530

    made the first server

---
+++ Nodejs/05-Server/miniServer.js
@@ -0,0 +1,26 @@
+// This is a mini -  multi route or multipage sever
+const fs = require("fs");
+const http = require("http");
+const { url } = require("inspector");
+const myMiniServer = http.createServer((req, res) => {
+  const log = `${Date.now()} :${req.url} New Req Received\n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home page");
+        break;
+      case "/about":
+        res.end("Hii there its Dave and its my first server");
+        break;
+      case "/contact":
+        res.end(
+          "You can email us on the email given below\n test123@gmail.com",
+        );
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
+});
+
+myMiniServer.listen(8001, () => console.log("Server Strated"));
