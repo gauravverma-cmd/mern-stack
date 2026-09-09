549040d9db444f3c0b63d82be83113b4d4be4818
gauravverma-cmd <gauravverma250609@gmail.com>
2026-09-09 22:48:56 +0530

    Url shortner

---
+++ Express_js/06-Url_Shortner/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const urlRoute = require("./routes/url")
+const {connectToMongoDb } = require("./connect")
+const PORT = 8001;
+const URL = require("./models/url");
+
+
+connectToMongoDb("mongodb://127.0.0.1:27017/urlShortner").then(() => console.log("MongoDb Connected"))
+app.use(express.json())
+app.get('/test',(req,res)=> {
+    return res.send("<h1>Hii this Server side rendering</h1>")
+})
+app.use("/url", urlRoute)
+app.get('/:shortId',async(req,res)=> {
+ const shortId = req.params.shortId;
+ const entry =  await URL.findOneAndUpdate({
+    shortId
+ },{
+    $push: {
+        visitHistory: {
+            timestamp : Date.now(), 
+        },
+    }
+ });
+ res.redirect(entry.redirectUrl)
+})
+app.listen(PORT,  ()=> console.log("Server Starts at : ", PORT));
